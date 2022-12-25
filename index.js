767785367ff4621334c98c95fb9a16b4afaa1c22
Tam Jid <tamjid.mdh@gmail.com>
2022-12-25 07:31:19 +0600

    fetch and show ebills

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
+import dotenv from "dotenv";
 import express from 'express';
-const app = express();
-const port = 3000;
+import contentfulClient from "./contentfulClient.js";
 
-app.get('/', (req, res) => {
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
     res.send('Root!');
 });
 
-app.get('/health', (req, res) => {
+app.get('/health', (_req, res) => {
     res.status(200).json({ 'message': 'Success!' });
 });
 
 app.post('/ebills/create', (req, res) => {
-    res.status(200).json({ 'message': 'Ebills create!' });
+    res.status(201).json({ 'message': 'Ebills create!' });
 });
-app.get('/ebills/index', (req, res) => {
-    res.status(200).json({ 'message': 'Ebills index!' });
+app.get('/ebills/index', async (_req, res) => {
+    let ebillsArr = [];
+    try {
+        await contentfulClient.getEntries().then(function (entries) {
+            //
+            entries.items.forEach(function (entry) {
+                if (entry.fields) {
+                    ebillsArr.push(entry.fields);
+                    console.log(entry.fields);
+                }
+            });
+        });
+        res.status(200).json({ ebills: ebillsArr });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 app.get('/ebills/show/:id', (req, res) => {
     res.status(200).json({ 'message': 'Ebills show!' });
 });
 app.put('/ebills/update/:id', (req, res) => {
-    res.status(200).json({ 'message': 'Ebills update!' });
+    res.status(203).json({ 'message': 'Ebills update!' });
 });
 app.delete('/ebills/destroy/:id', (req, res) => {
-    res.status(200).json({ 'message': 'Ebills destroy!' });
+    res.status(204).json({ 'message': 'Ebills destroy!' });
 });
 
 app.listen(port, () => {
