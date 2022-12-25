d933741b5f0738cca45302a523cbc201053044b8
Tam Jid <tamjid.mdh@gmail.com>
2022-12-25 07:36:49 +0600

    removed static keys

--- contentfulClient.js
+++ contentfulClient.js
@@ -1,10 +1,6 @@
 import contentful from 'contentful';
 
-const spaceId = process.env.SPACE_ID;
-const accessToken = process.env.ACCESS_TOKEN;
-console.log(spaceId, accessToken);
-
 export default contentful.createClient({
-    space: 'a56nkt3d6e5p',
-    accessToken: 'RTfMyYdwUa_l872vZS-5AxhhZHIc9kZjwxiEcqNsWRw',
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN,
 });
