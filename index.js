71ee58324293152719426443d3c28d5135901fb9
Tam Jid <tamjid.mdh@gmail.com>
2022-12-25 07:16:11 +0600

    routes draft

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Root!');
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ 'message': 'Success!' });
+});
+
+app.post('/ebills/create', (req, res) => {
+    res.status(200).json({ 'message': 'Ebills create!' });
+});
+app.get('/ebills/index', (req, res) => {
+    res.status(200).json({ 'message': 'Ebills index!' });
+});
+app.get('/ebills/show/:id', (req, res) => {
+    res.status(200).json({ 'message': 'Ebills show!' });
+});
+app.put('/ebills/update/:id', (req, res) => {
+    res.status(200).json({ 'message': 'Ebills update!' });
+});
+app.delete('/ebills/destroy/:id', (req, res) => {
+    res.status(200).json({ 'message': 'Ebills destroy!' });
 });
 
 app.listen(port, () => {
